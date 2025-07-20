31ee98f2e3a7b3dd5030d4fcb7dbb7f1198101ee
deepsoumya617 <soumyadeepghosh617@gmail.com>
2025-07-20 12:41:45 +0530

    feat(footer): enhance link styling with hover effects for better UX

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,20 @@
 export default function Footer() {
   return (
     <footer className="font-geist w-full border-t border-black/20 px-4 py-8 tracking-tight sm:px-8 lg:px-16">
-      <div className="mx-auto flex w-full max-w-4xl flex-col justify-between md:flex-row gap-4">
-        <h1 className="text-2xl font-black text-center">ねすと</h1>
-        <div className="flex gap-8 justify-center">
+      <div className="mx-auto flex w-full max-w-4xl flex-col justify-between gap-4 md:flex-row">
+        <h1 className="text-center text-2xl font-black">ねすと</h1>
+        <div className="flex justify-center gap-8">
           <div className="flex flex-col">
             <p className="font-medium">Product</p>
             <div className="mt-1 flex flex-col gap-0.5 text-sm text-black/80">
-              <a href="/changelog">Changelog</a>
-              <a href="https://github.com/deepsoumya617/nesto" target="_blank">
+              <a href="/changelog" className="hover:text-blue-500">
+                Changelog
+              </a>
+              <a
+                href="https://github.com/deepsoumya617/nesto"
+                target="_blank"
+                className="hover:text-blue-500"
+              >
                 Github
               </a>
             </div>
@@ -16,15 +22,23 @@ export default function Footer() {
           <div className="flex flex-col">
             <p className="font-medium">Support</p>
             <div className="mt-1 flex flex-col gap-0.5 text-sm text-black/80">
-              <a href="/contact">Contact Us</a>
-              <a href="/report">Report an Issue</a>
+              <a href="/contact" className="hover:text-blue-500">
+                Contact Us
+              </a>
+              <a href="/report" className="hover:text-blue-500">
+                Report an Issue
+              </a>
             </div>
           </div>
           <div className="flex flex-col">
             <p className="font-medium">Legal</p>
             <div className="mt-1 flex flex-col gap-0.5 text-sm text-black/80">
-              <a href="/privacy">Privacy Policy</a>
-              <a href="/terms">Terms of Service</a>
+              <a href="/privacy" className="hover:text-blue-500">
+                Privacy Policy
+              </a>
+              <a href="/terms" className="hover:text-blue-500">
+                Terms of Service
+              </a>
             </div>
           </div>
           {/* <div className="flex flex-col">
